4f7cd059b3fce4faca787f47ca939d3668f33ffa
Lucas Aguayo <lucasaguayorr@gmail.com>
2025-05-12 10:25:54 -0300

    Made the size of the squares adapt to the size of the container.

--- main.js
+++ main.js
@@ -1,10 +1,14 @@
-SIZE = 16;
+const DEFAULT_SIZE = 16;
+let inputSize = document.querySelector("#range").value;
 
 let containerOfDivs = document.querySelector(".container");
-for (let row = 0; row < SIZE; row++) {
-    for (let col = 0; col < SIZE; col++) {
-        let div = document.createElement("div");
-        containerOfDivs.appendChild(div);
-        div.classList.add("square")
+for (let row = 0; row < DEFAULT_SIZE; row++) {
+    let row = document.createElement("div");
+    row.classList.add("row");
+    containerOfDivs.appendChild(row);
+    for (let col = 0; col < DEFAULT_SIZE; col++) {
+        let square = document.createElement("div");
+        square.classList.add("square")
+        row.appendChild(square);
     }
 }
