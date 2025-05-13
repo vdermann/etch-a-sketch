0d8e38fa3aa35443b49a267ce39706a2c98e8741
Lucas Aguayo <lucasaguayorr@gmail.com>
2025-05-12 23:11:58 -0300

    Created a function that asks the current mode to draw (works with 'single color mode' and 'rainbow mode'). Added the class 'active' for the selected mode.

--- main.js
+++ main.js
@@ -1,13 +1,17 @@
 // Default Values.
 const DEFAULT_SIZE = 32;
-const DEFAULT_COLOR = "#e1e1e1";
+const DEFAULT_COLOR = "#1e1e1e";
 
 let currentSize = DEFAULT_SIZE;
 let currentColor = DEFAULT_COLOR;
 let currentMode = "";
 
+
+
 function changeSize(size) {
+    containerOfDivs.textContent = "";
     currentSize = size;
+    createBoard();
 }
 
 function changeColor(color) {
@@ -16,6 +20,27 @@ function changeColor(color) {
 
 function changeMode(mode) {
     currentMode = mode;
+    if (currentMode == "color") {
+        colorMode.classList.add("active");
+        rainbowMode.classList.remove("active");
+        grayMode.classList.remove("active");
+        eraserMode.classList.remove("active");
+    } else if (currentMode == "rainbow") {
+        colorMode.classList.remove("active");
+        rainbowMode.classList.add("active");
+        grayMode.classList.remove("active");
+        eraserMode.classList.remove("active");
+    } else if (currentMode == "gray") {
+        colorMode.classList.remove("active");
+        rainbowMode.classList.remove("active");
+        grayMode.classList.add("active");
+        eraserMode.classList.remove("active");
+    } else {
+        colorMode.classList.remove("active");
+        rainbowMode.classList.remove("active");
+        grayMode.classList.remove("active");
+        eraserMode.classList.add("active");
+    }
 }
 
 
@@ -30,9 +55,10 @@ const eraserMode = document.querySelector(".eraser-btn");
 
 const reset = document.querySelector(".reset");
 
+
 // Listeners.
 inputColor.addEventListener("input", (e) => changeColor(e.target.value));
-inputSize.addEventListener("input", (e) => changeSize(+e.target.value))
+inputSize.addEventListener("input", (e) => changeSize(+e.target.value));
 
 colorMode.addEventListener("click", () => changeMode("color"));
 rainbowMode.addEventListener("click", () => changeMode("rainbow"));
@@ -43,18 +69,51 @@ eraserMode.addEventListener("click", () => changeMode("eraser"));
 
 
 // Creates the grid.
-
 let containerOfDivs = document.querySelector(".container");
-for (let row = 0; row < currentSize; row++) {
-    let row = document.createElement("div");
-    row.classList.add("row");
-    containerOfDivs.appendChild(row);
-    for (let col = 0; col < currentSize; col++) {
-        let square = document.createElement("div");
-        square.classList.add("square")
-        row.appendChild(square);
+
+function createBoard() {
+    for (let row = 0; row < currentSize; row++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        containerOfDivs.appendChild(row);
+        for (let col = 0; col < currentSize; col++) {
+            let square = document.createElement("div");
+            square.classList.add("square");
+            row.appendChild(square);
+            square.addEventListener("mouseover", draw);
+        }
     }
 }
+
+function draw(e) {
+    if (currentMode == "color") {
+        e.target.style.backgroundColor = currentColor;
+    } else if (currentMode == "rainbow") {
+        e.target.style.backgroundColor = getRandomColor();
+    } else if (currentMode == "gray") {
+        e.target.style.backgroundColor = darkenGray(e);
+    }
+}
+
+// let i = 0;
+// function darkenGray(e) {
+//     e.target.style.backgroundColor = `rgb(0, 0, 0, ${i}%)`;
+//     i += 10;
+//     return;
+// }
+
+function getRandomColor() {
+    let getRed = Math.floor(Math.random() * 255);
+    let getGreen = Math.floor(Math.random() * 255);
+    let getBlue = Math.floor(Math.random() * 255);
+    return `rgb(${getRed}, ${getGreen}, ${getBlue})`;
+}
+
+
+window.addEventListener("load", () => {
+    createBoard();
+})
+
 
 
 
